c578a7f63ace7e9d71800769768c7e937f046c8a
Sarah Johnson <sarah@Sarahs-MBP.homenet.telecomitalia.it>
2018-11-18 18:20:25 +0100

    added clamp functionality

--- lodash/_.js
+++ lodash/_.js
@@ -1,6 +1,12 @@
+const _ = {
+  clamp(x, y, z) {
+    let lowerBound = Math.max(x, y);
+    let upperBound = Math.min(lowerBound, z);
+    return upperBound;
+  }
+};
 
-
-
+console.log(_.clamp(3, 1, 2));
 
 
 // Do not write or modify code below this line.
