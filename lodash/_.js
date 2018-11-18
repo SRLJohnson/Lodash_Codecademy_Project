320f2cdcfde5271f7038635e1b20c160dcfb2090
Sarah Johnson <sarah@Sarahs-MBP.homenet.telecomitalia.it>
2018-11-18 20:46:21 +0100

    added initial buggy _.inRange functionality

--- lodash/_.js
+++ lodash/_.js
@@ -3,11 +3,31 @@ const _ = {
     let lowerBound = Math.max(x, y);
     let upperBound = Math.min(lowerBound, z);
     return upperBound;
+  },
+  inRange(x, y, z) {
+    let startValue = y;
+    let endValue = z;
+    if (z === undefined) {
+      let endValue = y;
+      let startValue = 0;
+    } else {
+      let endValue = z;
+      let startValue = y;
+    }
+    if (startValue > endValue) {
+      let swapValue = startValue;
+      let startValue = endValue;
+      let endValue = swapValue;
+    }
+    if (x >= startValue && x <= endValue) {
+      return true
+    } else {
+      return false
+    }
   }
 };
 
-console.log(_.clamp(3, 1, 2));
-
+console.log(_.inRange(1, 2));
 
 // Do not write or modify code below this line.
 module.exports = _;
